4edb2c20ebdaa79d5f9ca9a2c96f18eb6638cd87
tan <77719969+tan-creator@users.noreply.github.com>
2022-12-05 19:40:57 +0700

    fix user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = 'Users';
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
